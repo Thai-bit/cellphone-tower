33ebea3b96407101c0f62b0acf91baa249e6d0d3
Boonthai Yan <56100316+Thai-bit@users.noreply.github.com>
2021-03-03 11:12:17 -0800

    Add comments

--- main.cpp
+++ main.cpp
@@ -1,40 +1,55 @@
+/*
+ *                      Program: Base Tower efficient Algorithm
+ *                      Purpose: Implementation for the greedy algorithm
+ *                      Author:  Boonthai Yan
+ *                      Date: March 03 2021
+ *                      Version: 1.0
+ */
+
 #include <iostream>
 #include <vector>
 #include <fstream>
-using namespace std;
 
 
 
-vector<int> sortHouse(vector<int> houses);
-void placeCellPhoneTower(const vector<int>&houses);
+//declaration for sort function
+std::vector<int> sortHouse(std::vector<int> houses);
+//delcaration for calculate the cellphone tower function
+void placeCellPhoneTower(const std::vector<int>&houses);
 
 int main() {
 
-
-    vector<int> houses = {};
-
-    ifstream myfile("housePosition.txt");
-    assert(myfile.is_open());
+    std::vector<int> houses = {};
     int temp = -1;
+    // read in the houses position from a file
+    std::ifstream myfile("housePosition.txt");
+    assert(myfile.is_open());
+
     while(true){
         myfile >> temp;
         houses.push_back(temp);
-        if (myfile.eof()) {
+        if (myfile.eof()){
             myfile.close();
             break;
         }
     }
-
+    //sort the house in ascending order
     houses = sortHouse(houses);
+    //calculate the minimum base towers need to cover all houses
     placeCellPhoneTower(houses);
-
 
     return 0;
 }
 
-void placeCellPhoneTower(const vector<int>& houses){
+/**
+ * This function will go through the houses position in the list
+ * Find the first house on the list
+ * put a base station to cover the house position
+ * go to the next house and need a base tower coverage
+ * @param houses
+ */
+void placeCellPhoneTower(const std::vector<int>& houses){
         int cellTowerNum = 0;
-        int nextUncoveredHouse  = 0;
         int cellCoverage = 0;
         //first house on sorted house list
         for(int house : houses){
@@ -45,13 +60,19 @@ void placeCellPhoneTower(const vector<int>& houses){
                 cellTowerNum++;
             }
         }
-        cout << "Cell Tower Count: " << cellTowerNum << endl;
+        std::cout << "The minimum cellphone base use: " << cellTowerNum << std::endl;
     }
 
 
 
-
-vector<int> sortHouse(vector<int> houses){
+ /**
+  * This function use to sort the house list if the house is not
+  * sorted yet.
+  * The house will sort in ascending order
+  * @param houses
+  * @return sorted houses list
+  */
+std::vector<int> sortHouse(std::vector<int> houses){
     int temp = 0;
     for(int i = 0; i < houses.size(); i++){
         for (int j=0; j < houses.size() - 1; j++){
